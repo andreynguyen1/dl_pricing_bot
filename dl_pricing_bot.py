30f9426bf94213b62309e209a390170867b29ff1
andreynguyen1 <161470307+andreynguyen1@users.noreply.github.com>
2024-02-28 01:45:29 +0300

    beta 1

--- dl_pricing_bot.py
+++ dl_pricing_bot.py
@@ -10,6 +10,10 @@ bot = telebot.TeleBot('7123896415:AAHaVQeFj4Pc9_zlxi3DuprdRH9RIO1qoK4')
 # ID файла Excel на Google Диске (из его общедоступной ссылки)
 file_id = '1fo3Wbb6noDILXL96qkodV4uVInZYZY-p'
 
+# SKU и тип цены, введенные пользователем
+#user_price_type = None
+user_sku = None
+
 # Функция загрузки данных из Excel-файла, расположенного на Google Диске
 def load_excel_data_from_google_drive(file_id):
     url = f'https://drive.google.com/uc?export=download&id={file_id}'
@@ -19,22 +23,28 @@ def load_excel_data_from_google_drive(file_id):
     data = {}
     for row in sheet.iter_rows(values_only=True):
         sku = row[0]
-        if sku == 'SKU':  # Пропускаем заголовок
-            continue
-        if sku == input_sku:  # Проверяем точное совпадение SKU
-            prices = {sheet.cell(row=sku_index, column=i).value: row[i] for i in range(1, len(row) - 1)}
-            comment = row[-1]
-            data[sku] = {'prices': prices, 'comment': comment}
-            break  # Прекращаем поиск после нахождения точного совпадения
+        if sku == user_sku:  # Проверяем, соответствует ли SKU введенному пользователем
+            price_column = 2 if user_price_type == sheet.cell(row=1, column=2).value else 3
+            price = row[price_column - 1]
+            comment = row[3] if len(row) >= 4 else None
+            data[sku] = {'price': price, 'comment': comment}
+            break
     return data
-
-# Загрузка данных из Excel-файла на Google Диске
-excel_data = load_excel_data_from_google_drive(file_id)
 
 # Обработка команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    global user_price_type
+    user_price_type = None
     bot.reply_to(message, "Привет! Я бот, который поможет тебе найти значения цен и комментариев по SKU и типу цены.")
+    bot.send_message(message.chat.id, "Выберите тип цены:", reply_markup=create_price_type_keyboard())
+
+# Функция создания клавиатуры с типами цен
+def create_price_type_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    price_types = ['Moscow', 'Region']  # Здесь добавьте необходимые типы цен
+    keyboard.add(*[types.KeyboardButton(price_type) for price_type in price_types])
+    return keyboard
 
 # Обработка текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -42,7 +52,7 @@ def handle_message(message):
     global user_price_type, user_sku
     if user_price_type is None:
         user_price_type = message.text
-        bot.reply_to(message, "Теперь введите SKU:")
+        bot.send_message(message.chat.id, "Теперь введите SKU:")
     elif user_sku is None:
         user_sku = message.text
         excel_data = load_excel_data_from_google_drive(file_id)
